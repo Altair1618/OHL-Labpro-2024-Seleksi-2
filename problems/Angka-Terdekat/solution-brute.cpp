d150eb22a0448d45ece856c5b59e3eb10cbcb7e4
Altair1618 <farhannabilsuryono3@gmail.com>
2024-07-07 14:12:51 +0700

    add Angka Terdekat solution

---
+++ problems/Angka-Terdekat/solution-brute.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define ll                      long long
+#define all(v)                  (v).begin(), (v).end()
+#define forn(var, n)            for (ll (var) = 0; (var) < (n); ++(var))
+#define forr(var, start, end)   for (ll (var) = (start); (var) < (end); ++(var))
+#define fori(var, start, end)   for (ll (var) = (start); (var) > (end); --(var))
+#define fora(var, obj)          for (auto (var) : (obj))
+#define MOD1                    (ll) (1e9 + 7)
+#define MOD9                    (ll) (998244353)
+#define prints(x)               cout << (x) << " "
+#define println(x)              cout << (x) << "\n"
+#define newl()                  cout << "\n"
+
+using namespace std;
+
+void solve() {
+    ll n; cin >> n;
+    vector<ll> a(n); fora(&x, a) cin >> x;
+
+    forn(i, n) {
+        ll ans = -1;
+
+        fori(j, i - 1, -1) {
+            if (a[j] < a[i]) {
+                ans = a[j]; break;
+            }
+        }
+
+        prints(ans);
+    }
+
+    newl();
+    return;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    ll t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
---
+++ problems/Angka-Terdekat/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define ll                      long long
+#define all(v)                  (v).begin(), (v).end()
+#define forn(var, n)            for (ll (var) = 0; (var) < (n); ++(var))
+#define forr(var, start, end)   for (ll (var) = (start); (var) < (end); ++(var))
+#define fori(var, start, end)   for (ll (var) = (start); (var) > (end); --(var))
+#define fora(var, obj)          for (auto (var) : (obj))
+#define MOD1                    (ll) (1e9 + 7)
+#define MOD9                    (ll) (998244353)
+#define prints(x)               cout << (x) << " "
+#define println(x)              cout << (x) << "\n"
+#define newl()                  cout << "\n"
+
+using namespace std;
+
+void solve() {
+    ll n; cin >> n;
+    
+    stack<ll> s;
+    forn(i, n) {
+        ll x; cin >> x;
+
+        while (!s.empty() && s.top() >= x) s.pop();
+        
+        if (s.empty()) prints(-1);
+        else prints(s.top());
+
+        s.push(x);
+    }
+
+    newl();
+    return;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    ll t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
